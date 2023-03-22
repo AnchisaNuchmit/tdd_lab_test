1d1614b6330b15fe50c67511b2ae8144ca97a33a
Anchisatk <117658112+AnchisaNuchmit@users.noreply.github.com>
2023-03-22 14:43:34 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,3 +9,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "Anchisa"}
+    
+    response = client.post("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "Anchisa post"}
+    
+ 
